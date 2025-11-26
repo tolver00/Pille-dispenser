f0edec45a7baa0b0f561245180865eaef0f8d245
tolver00 <tolversenn@gmail.com>
2025-11-26 11:16:24 +0100

    minor fixes

--- python_api.py
+++ python_api.py
@@ -14,7 +14,7 @@ DB_NAME =       os.getenv("DB_NAME")
 DB_USER =       os.getenv("DB_USER") 
 DB_PASSWORD =   os.getenv("DB_PASSWORD")
 
-
+# Create initial table if not exists
 def create_patients_table():
     try:
         conn_string = f"host={HOST} dbname={DB_NAME} user={DB_USER} password={DB_PASSWORD}"
@@ -42,15 +42,16 @@ def create_patients_table():
  
 # create_patients_table()
 
-def fetch_from_db():
+# Fetch row/rows from patients table
+def fetch_from_db(patient_id):
     conn = None
     cur = None
     try:
         conn_string = f"host={HOST} dbname={DB_NAME} user={DB_USER} password={DB_PASSWORD}"
         conn = psycopg2.connect(conn_string)
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM patients;")
-        records = cur.fetchall()
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute("SELECT * FROM patients where id = %s;", (patient_id,))
+        records = cur.fetchone()
         return records
     except Exception as e:
         print(f"database error: {e}")
@@ -60,8 +61,9 @@ def fetch_from_db():
         if conn is not None:
             conn.close()
 
-print(fetch_from_db())
+print(fetch_from_db(2))
 
+# Insert into patients table
 def insert_into_db(first_name, last_name, age, blood_type, allergies):
     conn = None
     cur = None
@@ -86,21 +88,32 @@ def insert_into_db(first_name, last_name, age, blood_type, allergies):
 
 # insert_into_db("lars", "larsen", 22, "a", "different allergy")
 
-
-""" app = APIFlask(__name__)
-class Patient(Schema):
+# Class for inserting patient data
+class PatientIn(Schema):
     first_name = String(required=True)
-    last_name = String(required=True)
-    age = Column 
+    last_namee = String(required=True)
+    age = Integer(required=True)
     blood_type = String(required=True)
     allergies = String(required=True)
+
+# Class for fetching patient
+class PatientOut(Schema):
+    first_name = String(required=True)
+    last_namee = String(required=True)
+    age = Integer(required=True)
+    blood_type = String(required=True)
+    allergies = String(required=True)
+
+# APIFlask setup
+app = APIFlask(__name__)
 
 @app.post('/add_patient')
 @app.input(PatientIn)
 def add_new_patient(json_data):
+    insert_into_db(json_data["first_name"], json_data["last_name"], json_data["age"], json_data["blood_type"], json_data["allergies"])
     return {'message': 'created'}, 201
 
-@app.get('/health_data')
-def get_health_data():
-    return 
- """
+@app.get('/fetch_patient')
+def get_patient_info():
+    fetch_from_db(patient_id)
+    
