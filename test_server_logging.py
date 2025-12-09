0e88d5c7d92fbcf3b8944aa604befbfef60ec6af
tolver00 <tolversenn@gmail.com>
2025-12-09 13:21:38 +0100

    update requirements

--- test_server_logging.py
+++ test_server_logging.py
@@ -4,7 +4,6 @@ import server_logging
 def test_resource_utilization():
         cpu, mem, disk, pids, uptime = server_logging.resource_utilization()
             
-        # Percentages should be in 0–100
         assert isinstance(cpu, (int, float))
         assert 0.0 <= cpu <= 100.0
 
@@ -14,10 +13,8 @@ def test_resource_utilization():
         assert isinstance(disk, (int, float))
         assert 0.0 <= disk <= 100.0
 
-        # pids should be a positive integer
         assert isinstance(pids, int)
         assert pids > 0
 
-        # uptime in hours should be positive
         assert isinstance(uptime, (int, float))
         assert uptime > 0
